e6bc503d0309565c1cad75e204f90d44d704a371
C1283459 Paulo Braga de Assis Junior <c1283459@interno.bb.com.br>
2020-11-04 11:37:25 -0300

    Click detalhe

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 import { Pokemon } from 'src/app/model/Pokemon';
 import { PokemonList } from 'src/app/model/PokemonList';
 import { Constant } from 'src/assets/Constant';
@@ -22,6 +23,11 @@ export class HomeComponent {
   functionToCallOnKeyUp(event){
     this.searchText = event
   }
+
+  detailPokemon(pokemon:Pokemon){
+    // this.router.navigateByUrl('/user');
+  }
+
 
   buscaPokemon() {
     this.http.get(Constant.url).subscribe(
